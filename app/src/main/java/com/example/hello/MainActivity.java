f4e5e8baf26042947e9eb26311384a81d8503881
breaktimeSeok <breaktime_kr@naver.com>
2020-07-18 18:54:36 +0900

    인터넷 접속 Intent 버튼 추가 전화 연결 Intent 버튼 추가

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtn1Clicked(View v){
         Toast.makeText(this, "확인1 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
+    }
+
+    public void onBtn2Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onBtn3Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
     }
 }
